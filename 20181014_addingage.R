9745eb88c0f172698d77dc3a581eb1b0b99612ed
Toru Takeuchi <torug3@kta.biglobe.ne.jp>
2018-10-14 06:05:23 +0900

    add age and make a new row

---
+++ 20181014_addingage.R
@@ -0,0 +1,28 @@
+#tidyverseの読み込み
+library(tidyverse)
+
+setwd("C:/Users/torug/Desktop/forR")
+lung <- read.csv("lung2017.csv")
+
+#生年月日を日付型に変換
+birth <- as.Date(lung$生年月日)
+
+#年齢を計算して列追加
+#格納場所を予約
+birthy <- numeric(length=length(birth))
+#繰り返し処理で計算
+for(i in 1:length(birth)) {
+birthy[i] <- c(length(seq(birth[i],current[i],by="year")))-1
+}
+
+#ageの列を追加
+lung <- lung %>% mutate(age=birthy)
+
+#ggplotでちょっとグラフ化
+gg <- ggplot(data=lung) 
+gg  +aes(x=age,fill=性別) +geom_histogram()
+
+gg <- ggplot(data=lung) 
+gg  +aes(x=age,fill=受診種別) +geom_histogram()+
+　　labs(x="年齢", y="人数")
+
